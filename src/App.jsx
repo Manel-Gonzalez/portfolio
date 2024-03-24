e0db04ddf343b63b84fe19ddda5e824357540ccd
Manel-Gonzalez <manel.gonzalez.rico@gmail.com>
2024-03-24 14:52:46 +0100

    responsive mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import diccionario from "./data/test.json";
 
 import { usePDF } from "react-to-pdf";
 import "./App.css";
+import { json } from "react-router";
 
 export default function App() {
   const [language, setLanguage] = useState("en");
@@ -18,11 +19,11 @@ export default function App() {
     .then(res => res[language])
     .then(json => setData(json)) */
   }, [language]);
-
+  console.log(jsonData.curriculum.downloadCV);
   return (
     <div className="">
-      <div ref={targetRef} className="bg-gray-100/50">
-        <div className="p-2 py-2 flex flex-row gap-2">
+      <div ref={targetRef} className="p-4 w-100">
+        <div className="p-4 py-2 flex flex-row gap-2">
           <button
             className=" bg-white p-2 shadow-lg shadow-gray-500/65 border border-slate-200 hover:bg-slate-100 cursor-pointer rounded-lg 
                                 transform transition duration-500 
@@ -49,45 +50,55 @@ export default function App() {
           </button>
         </div>
 
-        <div className="grid md:grid-rows-3 md:grid-flow-col grid-rows-2 grid-flow-col bg-white m-4 rounded-md shadow-lg shadow-gray-500/65 border border-slate-200">
-          <div className="w-fit p-4 md:w-100 md:row-span-3 col-span-1">
-            <div className="rotate-45 mx-8 my-8 w-70 h-32 rounded-x p-1 flex justify-center">
+        <div className="flex flex-col md:flex-row bg-white m-4 rounded-md shadow-lg shadow-gray-500/65 border border-slate-200">
+          <div className=" p-8 w-100 md:w-2/5 md:row-span-3 col-span-1">
+            <div className="w-100 rounded-x p-1 flex justify-center">
               <img
-                className="-rotate-90 w-100 rounded-xl p-1 shadow-inner shadow-slate-300   "
+                className="rounded-xl p-1"
                 src={jsonData.img}
               />
             </div>
           </div>
+          
+          <div className="md:w-3/5 p-4">
+            <div className=" ">
+              <h1 className="text-lg font-bold">{jsonData.name}</h1>
+              <h2 className="text-lg font-medium">{jsonData.titulation}</h2>
+            </div>
 
-          <div className=" mt-8 md:row-span-1 md:col-span-2 order-2 md:w-100 col-span-3">
-            <h1 className="text-lg font-bold">{jsonData.name}</h1>
-            <h2 className="text-lg font-medium">{jsonData.titulation}</h2>
+            <div className="p-2 md:p-4">
+              <p className="text-justify">{jsonData.about_me}</p>
+            </div>
           </div>
 
-          <div className=" p-2 md:row-span-2 md:col-span-2 col-span-4 order-1 md:order-2 w-100  ">
-            <p className="">{jsonData.about_me}</p>
-          </div>
         </div>
 
-        <div className="flex justify-end flex-row w-100   bg-white mx-4n rounded-md shadow-lg shadow-gray-500/65 border border-slate-200 ">
+        <div className="flex justify-between flex-row w-100   bg-white mx-4n rounded-md shadow-lg shadow-gray-500/65 border border-slate-200 ">
+          <div className="w-6 m-3 transform transition duration-500 
+            hover:scale-125 ">
+            <a href={jsonData.curriculum?.downloadCV} download>
+              <img src={jsonData.curriculum?.img} alt={jsonData.curriculum?.name} />
+            </a>
+         </div>
+         <div className="flex flex-row">
           {jsonData.banner?.map((banner_item) => (
             <div
               className="w-6 m-3 transform transition duration-500 
-            hover:scale-125  "
-            >
-              <a className=" " href={banner_item.url}>
+            hover:scale-125">
+              <a href={banner_item.url}>
                 <img
-                  className=""
                   src={banner_item.img}
                   alt={banner_item.name}
                 />
               </a>
             </div>
           ))}
+
+         </div>
         </div>
 
-        <div className="grid md:grid-cols-4 pb-12">
-          <div className=" m-4 rounded-md bg-white  shadow-lg shadow-gray-500/65 border border-slate-200 col-span-3">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-2/3 m-4 rounded-md  bg-white  shadow-lg shadow-gray-500/65 border border-slate-200 col-span-3">
             <div className="">
               <h2 className="my-12 m-4 font-semibold text-lg shadow-md  p-3 bg-white  rounded-md shadow-gray-500/65 border border-slate-200 ">
                 {jsonData[section1]?.section_titles.job_title}
@@ -99,7 +110,7 @@ export default function App() {
                       {job.company_name}
                     </h3>
                     <div className="w-20">
-                      <img className=" rounded-md" src={job.logo} alt={job.logo} />
+                      <img className="w-1/3" src={job.logo} alt={job.logo} />
                     </div>
                   </div>
                   <h4 className="font-medium text-lg">{job.position}</h4>
@@ -126,7 +137,7 @@ export default function App() {
                   <h3 className="font-bold text-xl h-fit align-middle">{e.name}</h3>
                   <div className="flex flex-row gap-4 align-middle items-center">
                   <h4 className=" font-medium text-lg h-fit align-middle">{e.center}</h4>
-                  <img className="w-16 rounded-md" src={e.logo} alt={e.logo} />
+                  <img className="w-12 rounded-md" src={e.logo} alt={e.logo} />
 
                   </div>
                   <h4>{e.location}</h4>
@@ -153,7 +164,7 @@ export default function App() {
             </div>
           </div>
 
-          <div className=" p-2 col-span-1  bg-white m-4 rounded-md shadow-lg shadow-gray-500/65 border border-slate-200">
+          <div className="md:w-1/3 p-2 col-span-1  bg-white m-4 rounded-md shadow-lg shadow-gray-500/65 border border-slate-200">
             {jsonData[section2]?.details.map((details) => (
               <div className=" flex flex-col m-8 p-4 bg-gray-100/20 rounded-md shadow-inner shadow-gray-500/65 border border-slate-200 ">
                 <p>{details.location}</p>
